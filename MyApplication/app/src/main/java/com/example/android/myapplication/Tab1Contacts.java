b674b132bb6818646feb6a5f3b026cba3a100f26
q-PC\q <yseolk@gmail.com>
2017-06-30 21:53:05 +0900

    tab, color

---
+++ MyApplication/app/src/main/java/com/example/android/myapplication/Tab1Contacts.java
@@ -0,0 +1,21 @@
+package com.example.android.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by q on 2017-06-30.
+ */
+
+public class Tab1Contacts extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1contacts, container, false);
+        return rootView;
+    }
+}
